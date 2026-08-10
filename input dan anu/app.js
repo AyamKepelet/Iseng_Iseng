0411d3b0c539053590a6a6e45f3a65dbb771034e
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-08-10 19:02:38 +0700

    update

--- input dan anu/app.js
+++ input dan anu/app.js
@@ -7,39 +7,39 @@ const rl = readline.createInterface({
 });
 
 if (!fs.existsSync("config.json")) {
-  rl.question(
-    `Enter database (file_name.db)
-`,
-    (name) => {
-      const db = new Database(name);
-      fs.writeFileSync(
-        "config.json",
-        JSON.stringify({ database: `${name}` }, null, 0),
-      );
-      rl.close();
-    },
-  );
+  rl.question(`masukkan nama database (file_name.db)`, (name) => {
+    const db = new Database(name);
+    fs.writeFileSync(
+      "config.json",
+      JSON.stringify({ database: `${name}` }, null, 0),
+    );
+    console.log("ketik node app.js lagi... ");
+    rl.close();
+  });
 } else {
   const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
   const db = new Database(config.database);
   db.exec(`CREATE TABLE IF NOT EXISTS Task (
         TaskId INTEGER PRIMARY KEY,
-        TaskName TEXT NOT NULL
-    );`)
-  console.log("Database already initialized");
-  rl.question("input your task! ", (tasks) => {
-          
-    const masukkanTeks = db.prepare(`INSERT INTO Task (TaskName) VALUES(?)
-      `)
-      masukkanTeks.run(tasks)
-    rl.question(`display the tasks y/n `, (content) => {
-      const resultsTable = db.prepare(`SELECT * FROM Task`)
-      const getTasks = resultsTable.all()
-      if(content === "y"){
-        console.log(getTasks)
-      }
-      rl.close()
-      db.close()
-    })
-  })
+        TaskName TEXT NOT NULL,
+        DueAt TEXT
+    );`);
+  console.log("database sudah berhasil di inisialisasi");
+  rl.question("masukkan tugas mu ", (tasks) => {
+    rl.question("deadline! (today/tomorrow,jam)", (jam) => {
+      const masukkanTeks =
+        db.prepare(`INSERT INTO Task (TaskName,DueAt) VALUES(?,?)
+      `);
+      masukkanTeks.run(tasks, jam);
+      rl.question(`tampilkan the tasks? y/n `, (content) => {
+        const resultsTable = db.prepare(`SELECT * FROM Task`);
+        const getTasks = resultsTable.all();
+        if (content === "y") {
+          console.log(getTasks);
+        }
+        rl.close();
+        db.close();
+      });
+    });
+  });
 }
