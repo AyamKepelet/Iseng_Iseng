6007c66b234f08b14535c65a6bec2bff40cbb22a
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-08-19 19:01:19 +0700

    bljr

--- bljr backend/server.mjs
+++ bljr backend/server.mjs
@@ -1,11 +1,16 @@
 import express from "express"
 const app = express()
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
 
 app.get("/", (req,res) => {
-    console.log(res.send("halo"));
-    res.send("test")
+    res.sendFile(path.join(__dirname, "./index.html"))
 })
 
-app.listen(5500,() => {
-    console.log(`http://localhost:5500`);
+
+app.listen(3000,() => {
+    console.log("http://localhost:3000");
 })
