26fe283e5df0be10478801a8cfe42ef2e00e77ca
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2026-09-01 17:34:30 +0700

    bljr

--- bljr backend/public/script.js
+++ bljr backend/public/script.js
@@ -4,20 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const User_inp2 = document.getElementById("User_inp2")
     
 
-    document.getElementById("btn-Result").addEventListener("submit",(e) => {
-
-        const User_value1 = User_inp1.values
-        const User_value2 = User_inp2.values
+    document.getElementById("btn-Result").addEventListener("click", async (e) => {
         e.preventDefault()
+    try{
+        
+
+        const User_value1 = User_inp1.value
+        const User_value2 = User_inp2.value        
+
+        console.log(User_value1);
+        console.log(User_value2);
+        
+        const response = await fetch("/forms", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                username: User_value1,
+                pekerjaan: User_value2
+            })
+        })
+        const users = await response.json()
         let li = document.createElement("li")
         const p1 = document.createElement("p")
-        p1.textContent = User_value1
+        p1.textContent = users.name
         const p2 = document.createElement("p")
-        p2.textContent = User_value2
-        resultENV.appendChild(li)
+        p2.textContent = users.pekerjaan
+        resultENV.append(li)
         li.appendChild(p1)
         li.appendChild(p2)
-        alert("test")
+    } catch(e){
+        console.error(error);
+    }
     })
 })
 
--- bljr backend/server.js
+++ bljr backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import Database from "better-sqlite3"
 const app = express()
 import path from "path"
 import { fileURLToPath } from "url"
@@ -6,14 +7,35 @@ import { fileURLToPath } from "url"
 const __fileName = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__fileName)
 
-app.use(express.static("public"))
+    app.use(express.static("public"))
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+const db = new Database("simpan.db")
+db.pragma("journal_mode = WAL")
+db.exec(`
+    CREATE TABLE IF NOT EXISTS Karyawan (
+        id INTEGER PRIMARY KEY,
+        Name TEXT NOT NULL,
+        Pekerjaan TEXT NOT NULL 
+    )
+    `)
+
+
+app.post("/forms", (req,res) => {
+    console.log("POST /forms terpanggil")
+    console.log(req.body);
+    
+    const result = db.prepare(`
+        INSERT INTO Karyawan(Name,Pekerjaan) VALUES(?,?)
+        `).run(req.body.username,req.body.pekerjaan)
+        console.log("INSERT: ", result);
+        
+
+    const users = db.prepare(`SELECT * FROM Karyawan`).all()
+    res.json(users)
 })
-app.get("/users", (req,res) => {
-    res.send("dwadwa")
-})
+
 
 app.listen(3000,() => {
     console.log("http://localhost:3000");
